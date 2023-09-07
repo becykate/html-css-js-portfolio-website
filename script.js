11552ae7867bd51e2a5c60b312a603a67d5621a3
Becca <beccahunt7@gmail.com>
2023-09-07 20:07:28 +0100

    Maybe Fin?
    Adding dark mode toggle

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const stylesheetLink = document.getElementById("stylesheet");
+
+  // Function to change the CSS file
+  function changeCSSFile(newCSSFileName) {
+    stylesheetLink.href = newCSSFileName;
+  }
+
+  
+
+  const toggle = document.getElementById("toggle");
+  toggle.addEventListener("change", function() {
+    if (toggle.checked) {
+        changeCSSFile("dark.css");
+    } else {
+        changeCSSFile("light.css");
+    }
+});
+});
