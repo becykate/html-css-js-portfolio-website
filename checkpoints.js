6dae1c8752a2a16eed4a5962daf18ad04d55c630
Becca <beccahunt7@gmail.com>
2025-01-27 23:11:11 +0000

    Updated map for 2025 trip

--- checkpoints.js
+++ checkpoints.js
@@ -4,7 +4,9 @@ const checkpoints = [
       "CheckpointName": "Tokyo Skytree",
       "CheckpointDescription": "Tokyo Skytree is a broadcasting and observation tower in Sumida, Tokyo.",
       "Latitude": 35.7101,
-      "Longitude": 139.8107
+      "Longitude": 139.8107,
+      "Category": "Activity"
+      
     },
     
     {
@@ -12,7 +14,8 @@ const checkpoints = [
       "CheckpointName": "Ueno Zoo",
       "CheckpointDescription": "The Ueno Zoo is a 14.3-hectare zoo, located in Taitō, Tokyo, Japan. It is Japan's oldest zoo, opened on March 20, 1882.",
       "Latitude": 35.712223,
-      "Longitude": 139.771118
+      "Longitude": 139.771118,
+      "Category": "Activity"
     },
       
     {
@@ -20,7 +23,8 @@ const checkpoints = [
       "CheckpointName": "Sensō-ji Temple",
       "CheckpointDescription": "Sensō-ji, is an ancient Buddhist temple located in Asakusa, Tokyo, Japan. It is Tokyo's oldest temple, and one of its most significant.",
       "Latitude": 35.7148,
-      "Longitude": 139.7967
+      "Longitude": 139.7967,
+      "Category": "Activity"
     },
       
       {
@@ -28,7 +32,8 @@ const checkpoints = [
       "CheckpointName": "Meiji Shrine",
       "CheckpointDescription": "Meiji Shrine is a Shinto shrine in Shibuya, Tokyo, that is dedicated to the deified spirits of Emperor Meiji and his wife, Empress Shōken.",
       "Latitude": 35.6764,
-      "Longitude": 139.6993
+      "Longitude": 139.6993,
+      "Category": "Activity"
     },
       
       {
@@ -36,7 +41,8 @@ const checkpoints = [
       "CheckpointName": "Shinjuku Gyoen National Garden",
       "CheckpointDescription": "Shinjuku Gyo-en is a large park and garden in Shinjuku and Shibuya, Tokyo, Japan.",
       "Latitude": 35.6852,
-      "Longitude": 139.7101
+      "Longitude": 139.7101,
+      "Category": "Activity"
     },
       
       {
@@ -44,7 +50,8 @@ const checkpoints = [
       "CheckpointName": "Imperial Palace",
       "CheckpointDescription": "The Imperial Palace is the main residence of the Emperor of Japan.",
       "Latitude": 35.6852,
-      "Longitude": 139.7528
+      "Longitude": 139.7528,
+      "Category": "Activity"
     },
 
     {
@@ -52,7 +59,8 @@ const checkpoints = [
       "CheckpointName": "Odaiba",
       "CheckpointDescription": "Accessed via the Rainbow Bridge or the futuristic Yurikamome train, Odaiba is a high-tech entertainment hub on an artificial island in Tokyo Bay.",
       "Latitude": 35.6206,
-      "Longitude": 139.7805
+      "Longitude": 139.7805,
+      "Category": "Activity"
     },
 
     {
@@ -60,7 +68,8 @@ const checkpoints = [
       "CheckpointName": "Kamakura Daibutsu",
       "CheckpointDescription": "The Great Buddha of Kamakura (鎌倉大仏, Kamakura Daibutsu) is a large bronze statue of Amitābha, located on the temple grounds. Including the base, it measures 13.35 metres (43.8 ft) tall and is the second tallest statue in Japan.",
       "Latitude": 35.3168,
-      "Longitude": 139.5357
+      "Longitude": 139.5357,
+      "Category": "Activity"
     },
     
     {
@@ -68,7 +77,8 @@ const checkpoints = [
       "CheckpointName": "Hokokuji Bamboo Forest",
       "CheckpointDescription": "Hokokuji Temple, is best known for the beautiful, small bamboo grove found behind the temple's main hall, which lies thick with over 2000 dark green bamboo stalks.",
       "Latitude": 35.3200,
-      "Longitude": 139.5692
+      "Longitude": 139.5692,
+      "Category": "Activity"
     },
 
     {
@@ -76,15 +86,19 @@ const checkpoints = [
       "CheckpointName": "Omoide Yokocho",
       "CheckpointDescription": "A historic alleyway, Omoide Yokocho is a foodie destination with old-world charm, serving up sushi, ramen, yakitori and more.",
       "Latitude": 35.69307,
-      "Longitude": 139.6995
+      "Longitude": 139.6995,
+      "Category": "Activity"
     },
 
     {
       "CheckpointID": 11,
-      "CheckpointName": "Miyajima",
-      "CheckpointDescription": " Miyajima is a small island in Hiroshima Bay. It is known for its forests and ancient temples. Just offshore, the giant, orange Great Torii Gate is partially submerged at high tide.",
-      "Latitude": 34.2732,
-      "Longitude": 132.3052
+      "CheckpointName": "HOTEL MYSTAYS Asakusa",
+      "CheckpointDescription": "Our first hotel in Tokyo, located in the vibrant area of Asakusa.",
+      "Latitude": 35.697283,
+      "Longitude": 139.768518,
+      "Category": "Hotel",
+      "StartDate": "2025-02-05",
+      "EndDate": "2025-02-20",
     },
 
     {
@@ -92,7 +106,8 @@ const checkpoints = [
       "CheckpointName": "Nara Park",
       "CheckpointDescription": " Nara Park is one of the must-see areas in Nara City. The vast area covers 660 hectares and is renowned for its free-roaming deer and world famous temples.",
       "Latitude": 34.6850,
-      "Longitude": 135.8430
+      "Longitude": 135.8430,
+      "Category": "Activity"
     },
 
     {
@@ -100,7 +115,8 @@ const checkpoints = [
       "CheckpointName": "Tōdai-ji Temple",
       "CheckpointDescription": "Todaiji (Great Eastern Temple) is one of Japan's most famous and historically significant temples and a landmark of Nara. The temple was constructed in 752 as the head temple of all provincial Buddhist temples of Japan and grew so powerful that the capital was moved away from Nara in 784 in order to lower the temple's influence on government affairs. Until recently, Todaijis main hall, the Daibutsuden (Big Buddha Hall), held the record as the world's largest wooden building, despite the fact that the present reconstruction of 1692 is only two thirds of the original temple halls size.",
       "Latitude": 34.6890,
-      "Longitude": 135.8398
+      "Longitude": 135.8398,
+      "Category": "Activity"
     },
 
     {
@@ -108,7 +124,8 @@ const checkpoints = [
       "CheckpointName": "Fushimi Inari Shrine",
       "CheckpointDescription": "Fushimi Inari is the most important of several thousands of shrines dedicated to Inari, the Shinto god of rice. Foxes are thought to be Inari's messengers, resulting in many fox statues across the shrine grounds.",
       "Latitude": 34.9677,
-      "Longitude": 135.7792
+      "Longitude": 135.7792,
+      "Category": "Activity"
     },
 
     {
@@ -116,7 +133,8 @@ const checkpoints = [
       "CheckpointName": "Arashiyama Bamboo Forest",
       "CheckpointDescription": "Kyoto's Sagano Bamboo Forest: This beautiful grove in western Kyoto is often featured on website lists highlighting the most beautiful woodlands in the world.",
       "Latitude": 35.0168,
-      "Longitude": 135.6713
+      "Longitude": 135.6713,
+      "Category": "Activity"
     },
 
     {
@@ -124,7 +142,8 @@ const checkpoints = [
       "CheckpointName": "Dotonbori",
       "CheckpointDescription": "The lively entertainment area of Dotonbori is Osaka’s most famous tourist destination and renowned for its gaudy neon lights, extravagant signage, and the enormous variety of restaurants and bars. ",
       "Latitude": 34.6687,
-      "Longitude": 135.5013
+      "Longitude": 135.5013,
+      "Category": "Activity"
     },
 
     {
@@ -132,7 +151,8 @@ const checkpoints = [
       "CheckpointName": "Hakone Onsen",
       "CheckpointDescription": "Hakone Onsen, or Hakone Hot Springs, is a general term for numerous thermal spas located in the town of Hakone.",
       "Latitude": 35.2332,
-      "Longitude": 139.1042
+      "Longitude": 139.1042,
+      "Category": "Activity"
     },
 
     {
@@ -140,10 +160,108 @@ const checkpoints = [
       "CheckpointName": "Owakudani, Hakone",
       "CheckpointDescription": "This area is one of the most famous tourist attractions in Hakone due to its volcanic activity and striking scenic views.",
       "Latitude": 35.241916,
-      "Longitude": 139.020667
+      "Longitude": 139.020667,
+      "Category": "Activity"
     },
 
+    {
+      "CheckpointID": 19,
+      "CheckpointName": "Henn na Hotel Sendai Kokubuncho",
+      "CheckpointDescription": "Our hotel in Sendai, once we begin our travels North.",
+      "Latitude": 38.262963,
+      "Longitude": 140.871618,
+      "Category": "Hotel",
+      "StartDate": "2025-02-20",
+      "EndDate": "2025-02-22",
+    },
 
+    {
+      "CheckpointID": 20,
+      "CheckpointName": "APA Hotel Akita Senshu Koen",
+      "CheckpointDescription": "Our hotel in Akita, a mountainous northern prefecture on Honshu, Japan's main island.",
+      "Latitude": 39.72196,
+      "Longitude": 140.12411,
+      "Category": "Hotel",
+      "StartDate": "2025-02-22",
+      "EndDate": "2025-02-24",
+    },
+
+    {
+      "CheckpointID": 21,
+      "CheckpointName": "ART HOTEL Aomori",
+      "CheckpointDescription": "Our hotel in Aomori, known for natural landscapes including volcanic mountain ranges.",
+      "Latitude": 40.82444,
+      "Longitude": 140.74,
+      "Category": "Hotel",
+      "StartDate": "2025-02-24",
+      "EndDate": "2025-02-26",
+    },
+
+    {
+      "CheckpointID": 22,
+      "CheckpointName": "Sapporo View Hotel Odori Park",
+      "CheckpointDescription": "Our hotel in Sapporo, capital of the mountainous northern Japanese island of Hokkaido, is famous for its beer, skiing and annual Sapporo Snow Festival featuring enormous ice sculptures.",
+      "Latitude": 43.058866,
+      "Longitude": 141.344878,
+      "Category": "Hotel",
+      "StartDate": "2025-02-26",
+      "EndDate": "2025-03-01",
+    },
+
+    {
+      "CheckpointID": 23,
+      "CheckpointName": "Hotel Sanrriott Shinsaibashi",
+      "CheckpointDescription": "Our hotel in Osaka, known for its modern architecture, nightlife and hearty street food.",
+      "Latitude": 34.6792,
+      "Longitude": 135.504,
+      "Category": "Hotel",
+      "StartDate": "2025-03-01",
+      "EndDate": "2025-03-04",
+    },
+
+    {
+      "CheckpointID":24,
+      "CheckpointName": "APA Hotel Himeji Ekikita",
+      "CheckpointDescription": "Our hotel in Himeji where we are staying to view Himeji Castle.",
+      "Latitude": 34.829300,
+      "Longitude": 134.693400,
+      "Category": "Hotel",
+      "StartDate": "2025-03-04",
+      "EndDate": "2025-03-06",
+    },
+
+    {
+      "CheckpointID":25,
+      "CheckpointName": "Vessel Hotel Campana Kyoto Gojo",
+      "CheckpointDescription": "Our hotel in Kyoto, famous for its numerous classical Buddhist temples, as well as gardens, imperial palaces, Shinto shrines and traditional wooden houses.",
+      "Latitude": 34.995886,
+      "Longitude": 135.761279,
+      "Category": "Hotel",
+      "StartDate": "2025-03-06",
+      "EndDate": "2025-03-09",
+    },
+
+    {
+      "CheckpointID":26,
+      "CheckpointName": "Onsen & Garden -Asante Inn-",
+      "CheckpointDescription": "Our hotel in Hakone, a mountainous town known for its hot springs resorts (onsen) and views of the iconic volcano Mount Fuji.",
+      "Latitude": 35.41746,
+      "Longitude": 139.33885,
+      "Category": "Hotel",
+      "StartDate": "2025-03-09",
+      "EndDate": "2025-03-11",
+    },
+
+    {
+      "CheckpointID":27,
+      "CheckpointName": "OHWA hostel minowa station",
+      "CheckpointDescription": "Our final hotel in Tokyo, famous for its numerous classical Buddhist temples, as well as gardens, imperial palaces, Shinto shrines and traditional wooden houses.",
+      "Latitude": 35.7286,
+      "Longitude": 139.7928,
+      "Category": "Hotel",
+      "StartDate": "2025-03-09",
+      "EndDate": "2025-03-11",
+    },
 
 
   ];
